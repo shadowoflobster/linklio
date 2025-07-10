d59f97d06d443c6354833ab296e6cdbf9b8bf4bf
shadowoflobster <guraspishvilileqso@gmail.com>
2025-07-10 16:09:47 +0400

    feat: add user registration logic in CreateUserService

--- src/main/java/com/linklio/linklio/application/service/UserServices/CreateUserService.java
+++ src/main/java/com/linklio/linklio/application/service/UserServices/CreateUserService.java
@@ -1,4 +1,36 @@
 package com.linklio.linklio.application.service.UserServices;
 
+import com.linklio.linklio.adapters.inbound.rest.dto.RegisterRequest;
+import com.linklio.linklio.application.ports.out.rolePorts.LoadRolePort;
+import com.linklio.linklio.application.ports.out.userPorts.SaveUserPort;
+import com.linklio.linklio.domain.model.Role;
+import com.linklio.linklio.domain.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+@Service
+@RequiredArgsConstructor
 public class CreateUserService {
+    private final SaveUserPort saveUserPort;
+    private final LoadRolePort loadRolePort;
+    private final PasswordEncoder passwordEncoder;
+
+    public User register(RegisterRequest request){
+        Role defaultRole = loadRolePort.findByName("USER")
+                .orElseThrow(() -> new RuntimeException("Role USER not found"));
+
+        User user = new User();
+        user.setUserName(request.getUserName());
+        user.setEmail(request.getEmail());
+        user.setPasswordHash(passwordEncoder.encode(request.getPassword()));
+        user.setVerified(false);
+        user.setRoles(new HashSet<>());
+        user.getRoles().add(defaultRole);
+
+        return saveUserPort.save(user);
+    }
+
 }
