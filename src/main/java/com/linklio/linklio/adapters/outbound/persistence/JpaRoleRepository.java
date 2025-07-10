467e107f4be6efaaae6edb9b82543bea99629ac8
shadowoflobster <guraspishvilileqso@gmail.com>
2025-07-10 16:11:04 +0400

    feat: findByRoleName method for JpaRoleRepository

--- src/main/java/com/linklio/linklio/adapters/outbound/persistence/JpaRoleRepository.java
+++ src/main/java/com/linklio/linklio/adapters/outbound/persistence/JpaRoleRepository.java
@@ -4,6 +4,9 @@ import com.linklio.linklio.adapters.outbound.persistence.entity.JpaRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface JpaRoleRepository extends JpaRepository<JpaRoleEntity,Long> {
+    Optional<JpaRoleEntity> findByRoleName(String roleName);
 }
