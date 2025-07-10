b4ce6f9b8a3cf8b3a2b7ae13c93a785586d0e63e
shadowoflobster <guraspishvilileqso@gmail.com>
2025-07-10 18:55:43 +0400

    feat: added LoginResponse

---
+++ src/main/java/com/linklio/linklio/application/service/UserServices/LoginResponse.java
@@ -0,0 +1,10 @@
+package com.linklio.linklio.application.service.UserServices;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+private String accessToken;
+}
