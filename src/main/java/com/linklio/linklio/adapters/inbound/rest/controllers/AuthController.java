daca94503364dc79ae25147363afc9b10a7439a3
shadowoflobster <guraspishvilileqso@gmail.com>
2025-07-10 19:08:13 +0400

    feat:added login endpoint method to AuthController

--- src/main/java/com/linklio/linklio/adapters/inbound/rest/controllers/AuthController.java
+++ src/main/java/com/linklio/linklio/adapters/inbound/rest/controllers/AuthController.java
@@ -1,7 +1,9 @@
 package com.linklio.linklio.adapters.inbound.rest.controllers;
 
+import com.linklio.linklio.adapters.inbound.rest.dto.LoginRequest;
 import com.linklio.linklio.adapters.inbound.rest.dto.RegisterRequest;
 import com.linklio.linklio.application.service.UserServices.CreateUserService;
+import com.linklio.linklio.application.service.UserServices.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 private final CreateUserService createUserService;
+private final LoginService loginService;
 
 @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegisterRequest request){
     createUserService.register(request);
     return ResponseEntity.ok("User Registered successfully");
+}
+
+@PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) throws Exception {
+    loginService.login(request);
+    return ResponseEntity.ok("User Logged in successfully");
 }
 
 }
