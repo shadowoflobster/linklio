60c27d4dc481b7842ba47c21ff3d92c2f3da71d5
shadowoflobster <guraspishvilileqso@gmail.com>
2025-07-11 20:12:19 +0400

    feat: added link and icon entities

---
+++ src/main/java/com/linklio/linklio/adapters/outbound/persistence/entity/JpaIconEntity.java
@@ -0,0 +1,25 @@
+package com.linklio.linklio.adapters.outbound.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class JpaIconEntity {
+    @Id
+    private String id;
+
+    @Column(name = "icon_url")
+    private String iconUrl;
+
+    @Column
+    private String description;
+}
---
+++ src/main/java/com/linklio/linklio/adapters/outbound/persistence/entity/JpaLinkEntity.java
@@ -0,0 +1,65 @@
+package com.linklio.linklio.adapters.outbound.persistence.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@SequenceGenerator(name = "link-seq-gen", sequenceName = "link-seq", allocationSize = 1)
+public class JpaLinkEntity {
+    @Id
+    @GeneratedValue(generator = "link-seq-gen", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "link_text")
+    private String linkText;
+
+    @Column
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private JpaUserEntity user;
+
+    @Column(name="bg_color")
+    private String bgColor;
+
+    @Column(name = "text_color")
+    private String textColor;
+
+    @Column(name = "border_color")
+    private String borderColor;
+
+    @Column(name = "hover_bg_color")
+    private String hoverBgColor;
+
+    @Column(name = "hover_border_color")
+    private String hoverBorderColor;
+
+    @Column(name = "font_style")
+    private String fontStyle;
+
+    @ManyToOne
+    @JoinColumn(name = "icon_id")
+    private JpaIconEntity icon;
+
+    @Column(name = "is_visible")
+    private boolean isVisible;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+}
