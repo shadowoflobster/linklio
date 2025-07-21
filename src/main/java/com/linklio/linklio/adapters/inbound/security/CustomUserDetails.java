337bd05eddb912e67a2ae86db1e2288d0fe943a8
shadowoflobster <guraspishvilileqso@gmail.com>
2025-07-21 19:11:20 +0400

    feat: added CUstomUserDetails

---
+++ src/main/java/com/linklio/linklio/adapters/inbound/security/CustomUserDetails.java
@@ -0,0 +1,40 @@
+package com.linklio.linklio.adapters.inbound.security;
+
+import com.linklio.linklio.domain.model.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+
+public class CustomUserDetails implements UserDetails {
+    @Getter
+    private Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user) {
+        this.id = user.getId();
+        this.username = user.getEmail();      // or getUsername(), depending on your domain
+        this.password = user.getPasswordHash();
+        this.authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                .collect(Collectors.toList());
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { return authorities; }
+
+    @Override public String getPassword() { return password; }
+    @Override public String getUsername() { return username; }
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
+
+}
