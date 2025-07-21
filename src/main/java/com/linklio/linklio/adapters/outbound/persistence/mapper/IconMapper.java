550fa0e781060532d0514ecc67a58a792a6659a8
shadowoflobster <guraspishvilileqso@gmail.com>
2025-07-21 19:12:56 +0400

    fix: added @Component annotation to iconmapper

--- src/main/java/com/linklio/linklio/adapters/outbound/persistence/mapper/IconMapper.java
+++ src/main/java/com/linklio/linklio/adapters/outbound/persistence/mapper/IconMapper.java
@@ -2,7 +2,9 @@ package com.linklio.linklio.adapters.outbound.persistence.mapper;
 
 import com.linklio.linklio.adapters.outbound.persistence.entity.JpaIconEntity;
 import com.linklio.linklio.domain.model.Icon;
+import org.springframework.stereotype.Component;
 
+@Component
 public class IconMapper {
     public Icon toDomain(JpaIconEntity entity){
         if (entity == null) return null;
