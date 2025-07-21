6f9ee8530e99530331b724e9c36dc5c9f2621aa3
shadowoflobster <guraspishvilileqso@gmail.com>
2025-07-21 15:33:19 +0400

    feat: added LinkRepositoryAdapter

--- src/main/java/com/linklio/linklio/adapters/outbound/persistence/LinkRepositoryAdapter.java
+++ src/main/java/com/linklio/linklio/adapters/outbound/persistence/LinkRepositoryAdapter.java
@@ -1,9 +1,9 @@
 package com.linklio.linklio.adapters.outbound.persistence;
 
-import com.linklio.linklio.adapters.outbound.persistence.entity.JpaUserEntity;
+import com.linklio.linklio.adapters.outbound.persistence.entity.JpaLinkEntity;
+import com.linklio.linklio.adapters.outbound.persistence.mapper.LinkMapper;
 import com.linklio.linklio.application.ports.out.linkPorts.SaveLinkPort;
 import com.linklio.linklio.domain.model.Link;
-import com.linklio.linklio.domain.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -12,11 +12,15 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 public class LinkRepositoryAdapter implements SaveLinkPort {
-    private JpaLinkRepository jpaLinkRepository;
+    private final JpaLinkRepository jpaLinkRepository;
+    private final LinkMapper linkMapper;
 
 
     @Override
     public Link save(Link link){
-        return new Link();
+        JpaLinkEntity linkEntity = linkMapper.toEntity(link);
+        JpaLinkEntity saved = jpaLinkRepository.save(linkEntity);
+        return linkMapper.toDomain(saved);
+
     }
 }
