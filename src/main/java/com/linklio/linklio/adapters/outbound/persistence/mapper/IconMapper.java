bf734c4a29907318ae40e74b5246809bd498d7bd
shadowoflobster <guraspishvilileqso@gmail.com>
2025-07-21 15:30:44 +0400

    feat:added IconMapper

---
+++ src/main/java/com/linklio/linklio/adapters/outbound/persistence/mapper/IconMapper.java
@@ -0,0 +1,17 @@
+package com.linklio.linklio.adapters.outbound.persistence.mapper;
+
+import com.linklio.linklio.adapters.outbound.persistence.entity.JpaIconEntity;
+import com.linklio.linklio.domain.model.Icon;
+
+public class IconMapper {
+    public Icon toDomain(JpaIconEntity entity){
+        if (entity == null) return null;
+
+        Icon icon = new Icon();
+        icon.setId(entity.getId());
+        icon.setIconUrl(entity.getIconUrl());
+        icon.setDescription(entity.getDescription());
+
+        return icon;
+    }
+}
