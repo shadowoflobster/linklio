e33e5df63c9469347ca04b2781117fffcbaaa7f9
shadowoflobster <guraspishvilileqso@gmail.com>
2025-07-21 15:32:19 +0400

    fix:in JpaRepository arguments, JpaIconRepository changed into JpaIconEntity

--- src/main/java/com/linklio/linklio/adapters/outbound/persistence/JpaIconRepository.java
+++ src/main/java/com/linklio/linklio/adapters/outbound/persistence/JpaIconRepository.java
@@ -1,8 +1,9 @@
 package com.linklio.linklio.adapters.outbound.persistence;
 
+import com.linklio.linklio.adapters.outbound.persistence.entity.JpaIconEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaIconRepository extends JpaRepository<JpaIconRepository,String> {
+public interface JpaIconRepository extends JpaRepository<JpaIconEntity,String> {
 }
