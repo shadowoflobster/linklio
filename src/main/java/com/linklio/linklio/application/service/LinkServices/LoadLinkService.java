dd46fd5976698a08bdcd6444b4c637959a42e894
shadowoflobster <guraspishvilileqso@gmail.com>
2025-08-02 18:40:42 +0400

    feat: added LoadLinkService and loadLinkById method

---
+++ src/main/java/com/linklio/linklio/application/service/LinkServices/LoadLinkService.java
@@ -0,0 +1,22 @@
+package com.linklio.linklio.application.service.LinkServices;
+
+import com.linklio.linklio.adapters.inbound.rest.dto.LinkResponse;
+import com.linklio.linklio.adapters.outbound.persistence.mapper.LinkMapper;
+import com.linklio.linklio.application.exceptions.LinkNotFoundException;
+import com.linklio.linklio.application.ports.out.linkPorts.LoadLinkPort;
+import com.linklio.linklio.domain.model.Link;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoadLinkService {
+    private final LoadLinkPort loadLinkPort;
+    private final LinkMapper linkMapper;
+
+    public LinkResponse loadLinkById(Long id){
+        Link link = loadLinkPort.loadById(id)
+                .orElseThrow(() -> new LinkNotFoundException(id));
+        return linkMapper.toResponse(link);
+    }
+}
