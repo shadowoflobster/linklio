8404df92e474ddb12cedaec1f5595de03fd4f491
shadowoflobster <guraspishvilileqso@gmail.com>
2025-08-02 18:36:36 +0400

    feat: overrided loadById and deleteById methods in  LinkRepositoryAdapter adapter

--- src/main/java/com/linklio/linklio/adapters/outbound/persistence/LinkRepositoryAdapter.java
+++ src/main/java/com/linklio/linklio/adapters/outbound/persistence/LinkRepositoryAdapter.java
@@ -2,15 +2,18 @@ package com.linklio.linklio.adapters.outbound.persistence;
 
 import com.linklio.linklio.adapters.outbound.persistence.entity.JpaLinkEntity;
 import com.linklio.linklio.adapters.outbound.persistence.mapper.LinkMapper;
+import com.linklio.linklio.application.ports.out.linkPorts.DeleteLinkPort;
+import com.linklio.linklio.application.ports.out.linkPorts.LoadLinkPort;
 import com.linklio.linklio.application.ports.out.linkPorts.SaveLinkPort;
 import com.linklio.linklio.domain.model.Link;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
-public class LinkRepositoryAdapter implements SaveLinkPort {
+public class LinkRepositoryAdapter implements SaveLinkPort, LoadLinkPort, DeleteLinkPort {
     private final JpaLinkRepository jpaLinkRepository;
     private final LinkMapper linkMapper;
 
@@ -21,5 +24,17 @@ public class LinkRepositoryAdapter implements SaveLinkPort {
         JpaLinkEntity saved = jpaLinkRepository.save(linkEntity);
         return linkMapper.toDomain(saved);
 
+    }
+
+    @Override
+    public Optional<Link> loadById(Long id){
+        return jpaLinkRepository.findById(id)
+                .map(linkMapper::toDomain);
+    }
+
+
+    @Override
+    public void deleteById(Long id){
+        jpaLinkRepository.deleteById(id);
     }
 }
