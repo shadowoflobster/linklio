c24abb1be593fe377e8322f61ec7403d5b017b34
shadowoflobster <guraspishvilileqso@gmail.com>
2025-08-02 18:33:26 +0400

    feat: added getlink rest method

--- src/main/java/com/linklio/linklio/adapters/inbound/rest/controllers/LinkController.java
+++ src/main/java/com/linklio/linklio/adapters/inbound/rest/controllers/LinkController.java
@@ -1,23 +1,31 @@
 package com.linklio.linklio.adapters.inbound.rest.controllers;
 
 import com.linklio.linklio.adapters.inbound.rest.dto.LinkRequest;
+import com.linklio.linklio.adapters.inbound.rest.dto.LinkResponse;
 import com.linklio.linklio.application.service.LinkServices.CreateLinkService;
-import com.linklio.linklio.domain.model.Link;
+import com.linklio.linklio.application.service.LinkServices.LoadLinkService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
 
 @RestController
 @RequestMapping("/api/link")
 @AllArgsConstructor
 public class LinkController {
     private CreateLinkService createLinkService;
+    private LoadLinkService loadLinkService;
+
+    @GetMapping("/{id}")
+    public LinkResponse getLink(@PathVariable Long id){
+        return loadLinkService.loadLinkById(id);
+    }
 
     @PostMapping("/add")
-    public Link createLink(@RequestBody LinkRequest linkRequest){
-        return createLinkService.createLink(linkRequest);
-
+    public LinkResponse createLink(@RequestBody LinkRequest linkRequest, Principal principal){
+        return createLinkService.createLink(linkRequest, principal.getName());
     }
+
+
 }
