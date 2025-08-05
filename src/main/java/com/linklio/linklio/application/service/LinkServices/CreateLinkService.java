b38c1a99541cc40af366739e8bbde65f974014d4
shadowoflobster <guraspishvilileqso@gmail.com>
2025-08-05 14:32:44 +0400

    fix: removed jpa repository injections, changed them with ports since services should not inject JPA repositories directly

--- src/main/java/com/linklio/linklio/application/service/LinkServices/CreateLinkService.java
+++ src/main/java/com/linklio/linklio/application/service/LinkServices/CreateLinkService.java
@@ -8,7 +8,9 @@ import com.linklio.linklio.adapters.outbound.persistence.mapper.IconMapper;
 import com.linklio.linklio.adapters.outbound.persistence.mapper.LinkMapper;
 import com.linklio.linklio.adapters.outbound.persistence.mapper.UserMapper;
 import com.linklio.linklio.application.exceptions.IconNotFoundException;
+import com.linklio.linklio.application.ports.out.linkPorts.LoadIconPort;
 import com.linklio.linklio.application.ports.out.linkPorts.SaveLinkPort;
+import com.linklio.linklio.application.ports.out.userPorts.LoadUserPort;
 import com.linklio.linklio.domain.model.Icon;
 import com.linklio.linklio.domain.model.Link;
 import com.linklio.linklio.domain.model.User;
@@ -20,25 +22,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CreateLinkService {
     private final SaveLinkPort saveLinkPort;
-    private final JpaIconRepository iconRepository;
-    private final IconMapper iconMapper;
+    private final LoadIconPort loadIconPort;
     private final LinkMapper linkMapper;
-    private final JpaUserRepository userRepository;
-    private final UserMapper userMapper;
+    private final LoadUserPort loadUserPort;
+
 
 
     public LinkResponse createLink(LinkRequest request, String email){
         Icon icon = null;
         if (request.getIconId() != null) {
-            icon = iconRepository.findById(request.getIconId())
-                    .map(iconMapper::toDomain)
+            icon = loadIconPort.findById(request.getIconId())
                     .orElseThrow(() -> new IconNotFoundException(request.getIconId()));
         }
 
         Link link = linkMapper.toDomain(request,icon);
 
-        User user = userRepository.findByEmail(email)
-                .map(userMapper::toDomain)
+        User user = loadUserPort.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(email));
         link.setUser(user);
 
