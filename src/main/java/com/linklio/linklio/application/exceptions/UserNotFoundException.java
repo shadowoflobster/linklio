50d7755d89bb80fed17ccb2f73f35393a65f8443
shadowoflobster <guraspishvilileqso@gmail.com>
2025-08-18 15:10:29 +0400

    feat: Overloaded UserNotFoundException to take Id as argument

--- src/main/java/com/linklio/linklio/application/exceptions/UserNotFoundException.java
+++ src/main/java/com/linklio/linklio/application/exceptions/UserNotFoundException.java
@@ -4,4 +4,8 @@ public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(String email){
         super("User not found with email: "+email);
     }
+
+    public UserNotFoundException(Long id){
+        super("User not found with id: "+id);
+    }
 }
