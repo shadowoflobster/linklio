38a89d6e3629be72e4249b5213ee97b9f031da2b
shadowoflobster <guraspishvilileqso@gmail.com>
2025-08-18 16:28:01 +0400

    feat: added updateLink endpoint handler method to LinkController

--- src/main/java/com/linklio/linklio/adapters/inbound/rest/controllers/LinkController.java
+++ src/main/java/com/linklio/linklio/adapters/inbound/rest/controllers/LinkController.java
@@ -2,8 +2,10 @@ package com.linklio.linklio.adapters.inbound.rest.controllers;
 
 import com.linklio.linklio.adapters.inbound.rest.dto.LinkRequest;
 import com.linklio.linklio.adapters.inbound.rest.dto.LinkResponse;
+import com.linklio.linklio.application.exceptions.LinkNotFoundException;
 import com.linklio.linklio.application.service.LinkServices.CreateLinkService;
 import com.linklio.linklio.application.service.LinkServices.LoadLinkService;
+import com.linklio.linklio.application.service.LinkServices.UpdateLinkService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,7 @@ import java.security.Principal;
 public class LinkController {
     private CreateLinkService createLinkService;
     private LoadLinkService loadLinkService;
+    private UpdateLinkService updateLinkService;
 
     @GetMapping("/{id}")
     public LinkResponse getLink(@PathVariable Long id){
@@ -25,6 +28,12 @@ public class LinkController {
     @PostMapping("/add")
     public LinkResponse createLink(@RequestBody LinkRequest linkRequest, Principal principal){
         return createLinkService.createLink(linkRequest, principal.getName());
+    }
+
+    @PutMapping("/update/{id}")
+    public LinkResponse updateLink(@PathVariable Long id, @RequestBody LinkRequest request, Principal principal){
+        return updateLinkService.updateLink(id,request)
+                .orElseThrow(() -> new LinkNotFoundException(id));
     }
 
 
