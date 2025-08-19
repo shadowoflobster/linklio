802bec578e77136f5f37383b1802873fcf47c6e4
shadowoflobster <guraspishvilileqso@gmail.com>
2025-08-19 14:45:26 +0400

    changed imports

--- src/main/java/com/linklio/linklio/application/ports/out/linkPorts/UpdateLinkPort.java
+++ src/main/java/com/linklio/linklio/application/ports/out/linkPorts/UpdateLinkPort.java
@@ -2,6 +2,7 @@ package com.linklio.linklio.application.ports.out.linkPorts;
 
 import com.linklio.linklio.adapters.inbound.rest.dto.LinkRequest;
 import com.linklio.linklio.adapters.inbound.rest.dto.LinkResponse;
+import com.linklio.linklio.domain.model.Link;
 
 import java.util.Optional;
 
