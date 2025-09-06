67e8967e6027e4cc7edf4c94778d1ed8746b80e8
shadowoflobster <guraspishvilileqso@gmail.com>
2025-09-06 16:35:16 +0400

    feat: added loadLinkByUserId to LoadLinkService;

--- src/main/java/com/linklio/linklio/application/service/LinkServices/LoadLinkService.java
+++ src/main/java/com/linklio/linklio/application/service/LinkServices/LoadLinkService.java
@@ -8,15 +8,25 @@ import com.linklio.linklio.domain.model.Link;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class LoadLinkService {
     private final LoadLinkPort loadLinkPort;
+
     private final LinkMapper linkMapper;
 
     public LinkResponse loadLinkById(Long id){
         Link link = loadLinkPort.loadById(id)
                 .orElseThrow(() -> new LinkNotFoundException(id));
         return linkMapper.toResponse(link);
+    }
+
+    public List<LinkResponse> loadLinkByUserId(Long userId){
+        List<Link> links = loadLinkPort.loadByUserId(userId);
+        return links.stream()
+                .map(linkMapper::toResponse)
+                .toList();
     }
 }
