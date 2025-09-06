004a99b67b29a09a147c6b9a6bc8a5d1af405557
shadowoflobster <guraspishvilileqso@gmail.com>
2025-09-06 16:33:51 +0400

    feat: added findByUserId method to JpaLinkRepository

--- src/main/java/com/linklio/linklio/adapters/outbound/persistence/JpaLinkRepository.java
+++ src/main/java/com/linklio/linklio/adapters/outbound/persistence/JpaLinkRepository.java
@@ -4,6 +4,9 @@ import com.linklio.linklio.adapters.outbound.persistence.entity.JpaLinkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JpaLinkRepository extends JpaRepository<JpaLinkEntity,Long> {
+    List<JpaLinkEntity> findByUserId(Long userId);
 }
