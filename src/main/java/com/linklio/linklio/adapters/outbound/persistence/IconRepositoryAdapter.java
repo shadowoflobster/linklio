99b027f3b33d3796ef9b1548f08470db26b5395b
shadowoflobster <guraspishvilileqso@gmail.com>
2025-09-10 23:05:17 +0400

    feat:Overrided findAll method for Icon

--- src/main/java/com/linklio/linklio/adapters/outbound/persistence/IconRepositoryAdapter.java
+++ src/main/java/com/linklio/linklio/adapters/outbound/persistence/IconRepositoryAdapter.java
@@ -6,6 +6,7 @@ import com.linklio.linklio.domain.model.Icon;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,8 +15,17 @@ public class IconRepositoryAdapter implements LoadIconPort {
     private final JpaIconRepository iconRepository;
     private final IconMapper iconMapper;
 
+    @Override
     public Optional<Icon> findById(String id) {
         return iconRepository.findById(id)
                 .map(iconMapper::toDomain);
+    }
+
+    @Override
+    public List<Icon> findAll(){
+        return iconRepository.findAll()
+                .stream()
+                .map(iconMapper::toDomain)
+                .toList();
     }
 }
