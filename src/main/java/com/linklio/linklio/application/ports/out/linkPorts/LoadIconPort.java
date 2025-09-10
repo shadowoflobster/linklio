3da4e1dc5fa76c4e71789d93f3e94b06447e905a
shadowoflobster <guraspishvilileqso@gmail.com>
2025-09-10 23:06:24 +0400

    feat:Added findAll method

--- src/main/java/com/linklio/linklio/application/ports/out/linkPorts/LoadIconPort.java
+++ src/main/java/com/linklio/linklio/application/ports/out/linkPorts/LoadIconPort.java
@@ -2,9 +2,11 @@ package com.linklio.linklio.application.ports.out.linkPorts;
 
 import com.linklio.linklio.domain.model.Icon;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LoadIconPort {
     Optional<Icon> findById(String id);
+    List<Icon> findAll();
 
 }
