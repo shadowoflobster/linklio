a818d3795008bb8d7aef501022f68c98d518d515
shadowoflobster <guraspishvilileqso@gmail.com>
2025-09-10 23:03:47 +0400

    fix: fixed DeleteLinkService, method now checks if link's owner users id is same as request senders id

--- src/main/java/com/linklio/linklio/application/service/LinkServices/DeleteLinkService.java
+++ src/main/java/com/linklio/linklio/application/service/LinkServices/DeleteLinkService.java
@@ -2,9 +2,12 @@ package com.linklio.linklio.application.service.LinkServices;
 
 import com.linklio.linklio.application.exceptions.LinkNotFoundException;
 import com.linklio.linklio.application.exceptions.UnauthorizedAccessException;
+import com.linklio.linklio.application.exceptions.UserNotFoundException;
 import com.linklio.linklio.application.ports.out.linkPorts.DeleteLinkPort;
 import com.linklio.linklio.application.ports.out.linkPorts.LoadLinkPort;
+import com.linklio.linklio.application.ports.out.userPorts.LoadUserPort;
 import com.linklio.linklio.domain.model.Link;
+import com.linklio.linklio.domain.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +16,15 @@ import org.springframework.stereotype.Service;
 public class DeleteLinkService {
     private final LoadLinkPort loadLinkPort;
     private final DeleteLinkPort deleteLinkPort;
+    private final LoadUserPort  loadUserPort;
 
     public void deleteLink(Long id, String email){
         Link link = loadLinkPort.loadById(id)
                 .orElseThrow(() -> new LinkNotFoundException(id));
+        User user = loadUserPort.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException(email));
 
-        if(!link.getUser().getEmail().equals(email)){
+        if(!link.getUser().getId().equals(user.getId())){
             throw new UnauthorizedAccessException();
         }
 
